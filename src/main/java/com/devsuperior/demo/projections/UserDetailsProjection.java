644a29433daf0174b310894cef9d89553ebe69bd
JOACIR VIANA <joacir.ines2014@gmail.com>
2024-05-22 21:57:08 -0400

    Authorization serve, Resource server, not tested

--- src/main/java/com/devsuperior/demo/projections/UserDetailsProjection.java
+++ src/main/java/com/devsuperior/demo/projections/UserDetailsProjection.java
@@ -2,4 +2,8 @@ package com.devsuperior.demo.projections;
 
 public interface UserDetailsProjection {
 
+	String getUsername();
+	String getPassword();
+	Long getRoleId();
+	String getAuthority();
 }
--- src/main/java/com/devsuperior/demo/services/UserService.java
+++ src/main/java/com/devsuperior/demo/services/UserService.java
@@ -1,12 +1,16 @@
 package com.devsuperior.demo.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.devsuperior.demo.entities.Role;
 import com.devsuperior.demo.entities.User;
+import com.devsuperior.demo.projections.UserDetailsProjection;
 import com.devsuperior.demo.repositories.UserRepository;
 
 @Service
@@ -18,10 +22,17 @@ public class UserService implements UserDetailsService {
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		
-		User user = repository.findByEmail(username);
-		if (user == null) {
+		List<UserDetailsProjection> result = repository.searchUserAndRolesByEmail(username);
+		if (result.size() == 0) {
 			throw new UsernameNotFoundException("User not found");
 		}
+		User user = new User();
+		user.setEmail(username);
+		user.setEmail(result.get(0).getPassword());
+		for (UserDetailsProjection projection : result) {
+			user.addRode(new Role(projection.getRoleId(), projection.getAuthority()));
+		}
+		
 		return user;
 	}
 
